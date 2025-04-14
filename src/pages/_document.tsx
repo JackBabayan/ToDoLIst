d285a05c2c525b031d3af233cc6cd76492fff7d4
s.babayan <s.babayan@notamedia.ru>
2025-04-14 17:10:54 +0400

    bg

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Saro Babayan To-Do List</title>
+        <title>Your To-Do List</title>
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="icon" href="/favicon-64x64.png" sizes="64x64" type="image/png" />
         <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
